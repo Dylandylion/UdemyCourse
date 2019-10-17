7871208739a11b3e35f3bc8fb0abe244c7855ff6
DylanN <dnguyen099@yahoo.com>
2019-10-17 23:39:06 +0000

    fixed create and created flash notices

--- rails_project/alpha-blog/app/controllers/articles_controller.rb
+++ rails_project/alpha-blog/app/controllers/articles_controller.rb
@@ -3,10 +3,19 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create
-        #render plain: params[:article].inspect
+        
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render "new"
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
     private 
--- rails_project/alpha-blog/config/routes.rb
+++ rails_project/alpha-blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   root "welcome#home"
+   root "articles#new"
    get 'about', to: "welcome#about"
    resources :articles
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
