b684a5aef820e9ab1cd63782c813c0066c5269d5
DylanN <dnguyen099@yahoo.com>
2019-10-19 01:03:43 +0000

    updating project once again

--- rails_project/alpha-blog/app/controllers/articles_controller.rb
+++ rails_project/alpha-blog/app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
     def new
         @article = Article.new
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         
         @article = Article.new(article_params)
@@ -12,6 +21,17 @@ class ArticlesController < ApplicationController
         else
             render "new"
         end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
+    
     end
     
     def show
