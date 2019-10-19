8ecb40eec53a92295b9baa041d2e7de3f925b553
DylanN <dnguyen099@yahoo.com>
2019-10-19 01:39:53 +0000

    Added delete function

--- rails_project/alpha-blog/config/routes.rb
+++ rails_project/alpha-blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   root "articles#index"
+   root "welcome#home"
    get 'about', to: "welcome#about"
    resources :articles
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
