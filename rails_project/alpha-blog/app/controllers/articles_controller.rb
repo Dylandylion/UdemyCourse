5c88fed9aa5a5a401da22d880868ffe086e7cc2a
DylanN <dnguyen099@yahoo.com>
2020-01-09 18:20:09 +0000

    styled show page

--- rails_project/alpha-blog/app/controllers/articles_controller.rb
+++ rails_project/alpha-blog/app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render "new"
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     def update
         
         if @article.update(article_params)
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render "edit"
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
     def destroy
         
         @article.destroy
-        flash[:notice] = "Article was successfully deleted."
+        flash[:danger] = "Article was successfully deleted."
         redirect_to articles_path
     end
     
