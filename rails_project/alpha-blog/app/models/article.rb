52c935170ff3b3d8d393ca677f7bc24ceb45c8dd
DylanN <dnguyen099@yahoo.com>
2020-01-21 18:04:32 +0000

    added association between user and articles, and added debugger view

--- rails_project/alpha-blog/app/models/article.rb
+++ rails_project/alpha-blog/app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: {minimum:3 , maxmimum:10}
     validates :description, presence: true, length: {minimum:3 , maxmimum:50}
+    validates :user_id, presence: true
 end
