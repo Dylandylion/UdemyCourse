adfd41da6f18b360fd7bc31907110868feadca0e
DylanN <dnguyen099@yahoo.com>
2020-01-27 17:08:12 +0000

    added edit function to user

--- rails_project/alpha-blog/app/controllers/users_controller.rb
+++ rails_project/alpha-blog/app/controllers/users_controller.rb
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
         render 'new'
         end
     end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your Account was Updated Successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
     
     private
     def user_params
