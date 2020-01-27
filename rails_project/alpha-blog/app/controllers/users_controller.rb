3b67e9c68a1f67d0bbb68df5eb3d05d44348740f
DylanN <dnguyen099@yahoo.com>
2020-01-27 21:31:39 +0000

    added more user features

--- rails_project/alpha-blog/app/controllers/users_controller.rb
+++ rails_project/alpha-blog/app/controllers/users_controller.rb
@@ -29,7 +29,9 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
-    
+    def index
+        @users = User.all
+    end
     private
     def user_params
             params.require(:user).permit(:username, :email, :password)
