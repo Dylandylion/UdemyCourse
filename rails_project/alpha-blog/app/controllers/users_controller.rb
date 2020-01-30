14c5e168e541e5c79474b21094e5c590feb7d190
DylanN <dnguyen099@yahoo.com>
2020-01-30 18:51:40 +0000

    added log in sign up, and styled nav.

--- rails_project/alpha-blog/app/controllers/users_controller.rb
+++ rails_project/alpha-blog/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+    
     def new
         @user = User.new
     end
@@ -13,11 +15,11 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
+        
     end
     
     def update
-        @user = User.find(params[:id])
+       
         if @user.update(user_params)
             flash[:success] = "Your Account was Updated Successfully"
             redirect_to articles_path
@@ -27,7 +29,7 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id])
+        
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
@@ -39,5 +41,8 @@ class UsersController < ApplicationController
     private
     def user_params
             params.require(:user).permit(:username, :email, :password)
+    end
+    def set_user
+        @user = User.find(params[:id])
     end
 end
--- rails_project/alpha-blog/config/routes.rb
+++ rails_project/alpha-blog/config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
    resources :articles
    get 'signup', to: "users#new"
    resources :users, except: [:new]
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
