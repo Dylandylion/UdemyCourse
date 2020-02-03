40fccff4ca9347889b1a485754e91cb50c855c5a
DylanN <dnguyen099@yahoo.com>
2020-02-03 18:41:39 +0000

    restricted UI

--- rails_project/alpha-blog/app/controllers/users_controller.rb
+++ rails_project/alpha-blog/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit,:update,:destroy]
     def new
         @user = User.new
     end
@@ -45,4 +46,11 @@ class UsersController < ApplicationController
     def set_user
         @user = User.find(params[:id])
     end
+    
+    def require_same_user
+            if current_user != User
+                flash[:danger] = "You can only edit own Account"
+                redirect_to root_path
+            end
+    end  
 end
