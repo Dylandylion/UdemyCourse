ad4d41ba6710fb91d6af388c0f9992d83c9f2a8b
DylanN <dnguyen099@yahoo.com>
2020-02-03 21:13:08 +0000

    added more admin functionality

--- rails_project/alpha-blog/app/controllers/users_controller.rb
+++ rails_project/alpha-blog/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit,:update,:destroy]\
-    
+    before_action :require_same_user, only: [:edit,:update,:destroy]
+    before_action :require_admin, only: [:destroy]
     def new
         @user = User.new
     end
@@ -41,6 +41,12 @@ class UsersController < ApplicationController
         @users = User.paginate(page: params[:page], per_page: 5)
     end
     
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted"
+        redirect_to users_path
+    end
     
     private
     def user_params
@@ -51,9 +57,14 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-            if current_user != User
+            if current_user != User and !current_user.admin?
                 flash[:danger] = "You can only edit own Account"
                 redirect_to root_path
             end
     end  
+    def require_admin
+        if logged_in? and !current_user.admin?
+            flash[:danger] = "Only Admin users can perform that action"
+        end
+    end
 end
